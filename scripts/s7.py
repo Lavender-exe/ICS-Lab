4a2414eeee1c0338f60d01ea4667d541f8c938ee
lavender-exe <l.m@securescape.cc>
2023-09-24 13:10:35 +0400

    Python fix

--- scripts/s7.py
+++ scripts/s7.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import snap7
-from keyboard import is_pressed
+import keyboard 
 from time import sleep
 
 print("S7 PLC Simulation Server")
@@ -13,17 +13,19 @@ def get_status():
   sleep(60)
   s7.get_status()
   
-def close_server():
-  print("[i] Press 'Q' to quit..")
-  if is_pressed('q'):
-    print("[!] Closing Server")
-    exit
-  elif is_pressed('Q'):
-    print("[!] Closing Server")
-    exit 
-  
 print("Server started at: 0.0.0.0:102")
+print("[i] Press 'q' to close server")
+
 while True:
-  s7.start()
-  get_status()
-  close_server()
+  try:
+    if keyboard.is_pressed("q"):
+      print("[!] Closing Server")
+      break
+    s7.start()
+    get_status()
+  except ModuleNotFoundError:
+    print("Install Keyboard Module - sudo pip install keyboard")
+    break
+  except Exception as e:
+    print(f"Error Caught: {e}")
+    break
